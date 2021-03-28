2a9d7c33e4e76f2b787ebf9a857571514a0df6cd
CP-WD <hkana2912@outlook.com>
2021-03-28 17:48:10 +0900

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   main.cpp
    
     Changes not staged for commit:
    	deleted:    tempmain.cpp
    
     Untracked files:
    	.vscode/

--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
-# include <iostream>
-# include <random>
-# include <chrono>
-# include <thread>
-# include <stdlib.h>
-# include <windows.h>
-# include <conio.h>
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
 
 # define HEIGHT (25) // 4+20+1
 # define WIDTH (12)  // 1+10+1
 
 # define SPEED (100)
+# define DELETE_POINT (100)
 
 
-int stage[HEIGHT][WIDTH];
-int next[7];
-
-
-int randint (int min, int max);
-int landing (void);
-int operation (int kind);
-int landing (void);
-int deleteLine(void);
-void drop (void);
-void clear (void);
-void moveMino (int dx, int dy);
-void stageInit (void);
-void decisionNext (void);
-void writeStage (void);
-void writeStageNum (void);
-void oneToZero (void);
-void changeStatic (void);
-void sleep (int s);
-void moveMino (void);
-void placeMino(int kind);
-int turnMino(int lr);
-
-
-
+int stage[HEIGHT][WIDTH], next[7];
+int point = 0;
 
 enum KIND_OF_MINO
 {
@@ -49,36 +29,61 @@ enum KIND_OF_MINO
     T_MINO
 };
 
-
+int randint (int min, int max); // 乱数と言いつつ一定の値（dekita）
+int landing (void);
+int operation (int kind);
+int landing (void);
+int deleteLine (void); // 消え方の挙動がおかしい(多分直った)
+void drop (void);
+void clear (void);
+int moveMino (int dx, int dy); // 横から壁やミノに刺せる(直った)
+void stageInit (void);
+void decisionNext (void);
+void writeStage (void);
+void writeStageNum (void);
+void oneToZero (void);
+void changeStatic (void);
+void sleep (int s);
+void placeMino (int kind);
+int turnMino (void);
 
 
 
 int main (void)
 {
+    srand(time(NULL));
     int end = 0;
 
     stageInit ();
-    writeStage();
-    decisionNext();
+    writeStage ();
+    decisionNext ();
 
-    while(end != 1)
+    while (1)
     {
+        int count = 0;
+
         for (int i=0;i<7;i++)
         {
-            end = operation(next[i]);
+            end = operation (next[i]);
+
+            for (int i=1;i<WIDTH-1;++i)
+            {
+                if (stage[4][i] == 2)
+                {
+                    std::cout << "Game Over";
+                    return 0;
+                }
+            }
 
             for (int j=0;j<4;++j)
             {
-                deleteLine();
+                count += deleteLine ();
             }
+            point += pow(DELETE_POINT ,count);
         }
-        decisionNext();
+        decisionNext ();
     }
-
-    std::cout << "Game Over" << std::endl;
 }
-
-
 
 
 
@@ -112,12 +117,10 @@ void stageInit (void)
 }
 
 
-int randint(int min, int max)
+int randint (int min, int max)
 {
-    std::random_device rnd;
-    std::mt19937 mt(rnd ());
-    std::uniform_int_distribution<> rand100 (min, max);
-    return rand100 (mt);
+    int s = rand() % (max+1) + min;
+    return s;
 }
 
 
@@ -149,21 +152,26 @@ void writeStage (void)
             switch (stage[i][j])
             {
                 case 0:
-                    std::cout << "  ";
+                    std::cout << "・";
                     break;
 
                 case 1:
-                    std::cout << "[]";
+                    std::cout << "■";
+                    break;
+                
+                case 2:
+                    std::cout << "■";
                     break;
 
                 default:
-                    std::cout << "::";
+                    std::cout << "□";
                     break;
 
             }
         }
         std::cout << std::endl;
     }
+    std::cout << point << std::endl;
 }
 
 
@@ -209,12 +217,11 @@ void changeStatic (void)
 
 void sleep (int s)
 {
-    //std::this_thread::sleep_for(std::chrono::milliseconds(s));
     Sleep(s);
 }
 
 
-void moveMino(int dx, int dy)
+int moveMino (int dx, int dy)
 {
     int coordinate[8] = {0,0,0,0,0,0,0,0}, count = 0;
 
@@ -224,84 +231,90 @@ void moveMino(int dx, int dy)
         {
             if (stage[y][x] == 1)
             {
+
                 coordinate[count] = y+dy;
                 coordinate[count+1] = x+dx;
-                stage[y][x] = 0;
                 count += 2;
             }
         }
     }
 
-    
+    for (int i=0;i<8;i+=2)
+    {
+        if (stage[coordinate[i]][coordinate[i+1]] != 1 && stage[coordinate[i]][coordinate[i+1]] != 0)
+        {
+            return 0;
+        }
+    }
+
     oneToZero();
 
-    
     stage[coordinate[0]][coordinate[1]] = 1;
     stage[coordinate[2]][coordinate[3]] = 1;
     stage[coordinate[4]][coordinate[5]] = 1;
     stage[coordinate[6]][coordinate[7]] = 1;
+
+    return 0;
 }
 
 
 int operation (int kind)
 {
-    for (int i=1;i<WIDTH-1;++i)
-    {
-        if (stage[4][i] == 2)
-        {
-            sleep(1000);
-            return 1;
-        }
-    }
-    
-    placeMino(kind);
-    clear();
-    writeStage();
-    sleep(SPEED);
+
+    placeMino (kind);
+    clear ();
+    writeStage ();
+    sleep (SPEED);
 
     while(1)
     {
-        if (landing())
+        if (landing ())
         {
-            changeStatic();
-            clear();
-            writeStage();
+            changeStatic ();
+            clear ();
+            writeStage ();
             break;
         }
 
 
 
-        clear();
-        moveMino(0, 1);
+        clear ();
+        moveMino (0, 1);
 
-        writeStage();
-        sleep(SPEED);
+        writeStage ();
+        sleep (SPEED);
 
-        if(kbhit())
+        if (kbhit ())
         {
-            int key = getch();
+            int key = getch ();
 
-			if (key == 108)//L
+            if (key == 105) //I
             {
-				moveMino(1, 0);
-                clear();
-                writeStage();
+                drop ();
+                clear ();
+                writeStage ();
+            }
+
+			else if (key == 108) //L
+            {
+				moveMino (1, 0);
+                clear ();
+                writeStage ();
 
 			}
 
-            else if (key == 106)//J
+            else if (key == 106) //J
             {
-                moveMino(-1, 0);
-                clear();
-                writeStage();
+                moveMino (-1, 0);
+                clear ();
+                writeStage ();
             }
 
-            else if (key == 107)//K
+            else if (key == 107) //K
             {
-                //drop();
-                turnMino(1);
-                clear();
-                writeStage();
+                turnMino ();
+                clear ();
+                writeStage ();
             }
 		}
     }
@@ -310,7 +323,7 @@ int operation (int kind)
 }
 
 
-void placeMino(int kind)
+void placeMino (int kind)
 {
     switch (kind)
     {
@@ -345,10 +358,10 @@ void placeMino(int kind)
 }
 
 
-void clear(void)
+void clear (void)
 {
-    //std::system("clear");
-    system("cls");
+    //std::system ("clear");
+    system ("cls");
 }
 
 
@@ -363,7 +376,6 @@ int landing (void)
                 if (stage[i+1][j] != 1 && stage[i+1][j] != 0)
                 {
                     return 1;
-                    break;
                 }
             }
         }
@@ -375,14 +387,15 @@ int landing (void)
 
 void drop (void)
 {
-    int coordinate[8] = {0,0,0,0,0,0,0,0}, count;
+    int coordinate[8] = {0,0,0,0,0,0,0,0}, count, d=0;
     int dx=0, dy=1;
 
     while (1)
     {
         count = 0;
-        if (landing())
+        if (landing ())
         {
+            point += d*10;
             break;
         }
 
@@ -406,28 +419,32 @@ void drop (void)
         stage[coordinate[2]][coordinate[3]] = 1;
         stage[coordinate[4]][coordinate[5]] = 1;
         stage[coordinate[6]][coordinate[7]] = 1;
+
+        d += 1;
     }
 }
 
 
-int deleteLine(void)
+int deleteLine (void)
 {
-    int i, j, k, l, m, count;
+    int i, j, k, l, m, count, returnNum = 0;
 
 
     for (i=0;i<HEIGHT;++i)
     {
-        for (j=0;j<WIDTH;++j)
+        for (j=1;j<WIDTH-1;++j)
         {
             if (stage[i][j] == 2)
             {
-                count+=1;
+                count += 1;
             }
         }
 
 
         if (count == 10)
         {
+            returnNum = 1;
+
             for (m=1;m<WIDTH-1;++m)
             {
                 stage[i][m] = 0;
@@ -438,8 +455,7 @@ int deleteLine(void)
             {
                 for (l=1;l<WIDTH-1;++l)
                 {
-                    if (stage[k-1][l] == 2) stage[k][l] = 2;
-                    else if (stage[k-1][l] = 0) stage[k][l] = 0;
+                    stage[k][l] = stage[k-1][l];
                 }
             }
 
@@ -448,11 +464,11 @@ int deleteLine(void)
         count = 0;
     }
 
-    return 0;
+    return returnNum;
 }
 
 
-int turnMino(int lr)
+int turnMino (void)
 {
     int maxI=0, minI = 100, maxJ=0, minJ=100;
     int dx=0, dy=0;
@@ -498,6 +514,14 @@ int turnMino(int lr)
     {
         for (int x=0;x<tempWidth;++x)
         {
+			if (tempStage[y][x] != 1 && tempStage[y][x] != 0) return 0;
+        }
+    }
+
+    for (int y=0;y<tempWidth;++y)
+    {
+        for (int x=0;x<tempWidth;++x)
+        {
 			tempStageTwo[y][x] = tempStage[tempWidth-1-x][y];
         }
         std::cout << "\n";
@@ -512,4 +536,17 @@ int turnMino(int lr)
         }
     }
     return 0;
+}
+
+
+int pow (int x, int y)
+{
+    int ans = 1;
+
+    for (int i=0;i<y;i++)
+    {
+        ans *= x;
+    }
+
+    return ans;
 }
